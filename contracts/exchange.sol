abd24571e8b63664833efdeaa25bc61baf4ceb2d
Sergio Bugallo <128857170+sejabu@users.noreply.github.com>
2024-12-07 09:36:53 -0300

    small insignificant changes

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -29,64 +29,9 @@ contract exchange is Ownable {
         require(_USDT != address(0) && _products != address(0), "Invalid token addresses");
         require(_USDT != _products, "Tokens must be different");
         USDT = IERC20(_USDT);
-        products = IERC20(_products); 
+        products = IERC1155(_products); 
     }
 
-    
-
-    /** TODAVIA EN PROCESO!!!!
-     *
-     * @dev Swaps Products for USDT.
-     * @param amountAIn Amount of TokenA to swap.
-     */
-    function swapAforB(uint256 amountAIn) external {
-        require(amountAIn > 0, "Amount must be greater than zero");
-        require(tokenA.allowance(msg.sender, address(this)) >= amountAIn, "TokenA allowance too low");
-
-        uint256 amountBOut = getAmountOut(amountAIn, reserveA, reserveB);
-        
-        tokenA.transferFrom(msg.sender, address(this), amountAIn);
-        tokenB.transfer(msg.sender, amountBOut);
-
-        reserveA += amountAIn;
-        reserveB -= amountBOut;
-
-        emit TokensSwapped(msg.sender, address(tokenA), amountAIn, address(tokenB), amountBOut);
-    }
-
-    /**
-     * @dev Swaps TokenB for TokenA.
-     * @param amountBIn Amount of TokenB to swap.
-     */
-    function swapBforA(uint256 amountBIn) external {
-        require(amountBIn > 0, "Amount must be greater than zero");
-        require(tokenB.allowance(msg.sender, address(this)) >= amountBIn, "TokenB allowance too low");
-
-        uint256 amountAOut = getAmountOut(amountBIn, reserveB, reserveA);
-
-        tokenB.transferFrom(msg.sender, address(this), amountBIn);
-        tokenA.transfer(msg.sender, amountAOut);
-
-        reserveB += amountBIn;
-        reserveA -= amountAOut;
-
-        emit TokensSwapped(msg.sender, address(tokenB), amountBIn, address(tokenA), amountAOut);
-    }
-
-    /**
-     * @dev Calculates the output amount using the constant product formula.
-     * @param amountIn Input token amount.
-     * @param reserveIn Input token reserve.
-     * @param reserveOut Output token reserve.
-     */
-    function getAmountOut(uint256 amountIn, uint256 reserveIn, uint256 reserveOut) internal pure returns (uint256) {
-        require(reserveIn > 0 && reserveOut > 0, "Invalid reserves");
- 
-        uint256 numerator = amountIn * reserveOut;
-        uint256 denominator = reserveIn + amountIn;
-        return numerator / denominator;
-    }
-
-   
+  
 
 }
