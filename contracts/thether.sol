866a19f772832a5edf6e583f930acfed65549772
Lujan Tissera <72819517+lujantissera@users.noreply.github.com>
2024-12-07 22:05:16 +0100

    Cambios a main

--- contracts/thether.sol
+++ contracts/thether.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Thether is ERC20 {
+contract thether is ERC20 {
     constructor() ERC20("Thether", "USDT") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
